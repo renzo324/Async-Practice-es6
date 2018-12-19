144227a9676491443e47f2d86495d3b8ace39a76
renzo324 <finnthehuman324@gmail.com>
2018-12-19 22:08:39 +0800

    change to react

--- app/index.js
+++ app/index.js
@@ -1,22 +1,32 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('we are live');
-let request = obj => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open(obj.method || "GET", "https://cors-anywhere.herokuapp.com/remoteok.io/api");
-        if (obj.headers) {
-            Object.keys(obj.headers).forEach(key => {
-                xhr.setRequestHeader(key, obj.headers[key]);
-            });
+const api_key= '11ec91bd52bf60e23b0b9f3020b91b3c';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            city:'San Francisco',
+            description: ''
         }
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject(xhr.statusText);
-            }
-        };
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send(obj.body);
-    });
-};
+    }
+    componentDidMount(){
+        this.grabWeather(this.state.city);
+    }
+    grabWeather(city){
+        fetch(`http://api.openweathermap.org/data/2.5/weather?APPID+${api_key}&q=${city}`)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({description: json.weathes[0].description})
+            });
+    }
+    render(){
+        return(
+            `<h1> Weather report for {this.state.city} </h1>
+            <h2>{this.state.description}</h2>`
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
